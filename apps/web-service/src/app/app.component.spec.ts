8b8e4655424a70f6c5a35efba3789a592d3ed28d
Alex-v-p <r0989526@student.thomasmore.be>
2026-04-08 21:39:51 +0200

    added basic nx setup for all services, switched from jasmin to vitest

---
+++ apps/web-service/src/app/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { describe, it, expect } from 'vitest';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('should create the app', async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    expect(app).toBeTruthy();
+  });
+});
